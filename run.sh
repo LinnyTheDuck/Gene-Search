a9ebd12d21641c1f0f61586862feafd9c1446978
Linny <github@quackers.slmail.me>
2022-08-11 15:17:49 +1200

    bash script

---
+++ run.sh
@@ -0,0 +1,16 @@
+if [ -z "$1" ]
+then
+    # to use: ./run.sh contig00028.txt
+    printf "How To Use This Program:
+./run.sh <contig text file>
+Will print all output to the console, add \"> file\" at the end to direct all standard output to a file
+"
+else
+    # go up a directory to access all the awk programs
+    cd ..
+    # will run all the awk files together
+    # extracts, finds the genes, translates into peptide sequence
+    awk -f extract.awk $0 | awk -f isolate_subsequence.awk | awk -f translate.awk
+    # extracts, converts to reverse compliment, finds the genes, translates into peptide sequence
+    awk -f extract.awk $0 | awk -f reverse_compliment.awk | awk -f isolate_subsequence.awk | awk -f translate.awk
+fi
