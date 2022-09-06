5114526bb5a23d1de2a9f119866f06bdeec94e88
Linny <github@quackers.slmail.me>
2022-09-06 13:03:58 +1200

    everything working

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,32 +1,46 @@
+# fail on the first error
+set -e 
+
 if [ -z "$1" ]
 then
     # to use: ./run.sh contig00028.txt
-    printf "How To Use This Program:
-./run.sh <contig text file>
-Will print all output to the console, add \"> file\" at the end to direct all standard output to a file
+    printf "Usage: ./run.sh <contig text file> <option>
+
+By default a file with each gene and corresponding protien sequence will be produced.
+
+Options:
+-l      also generate peptides of length 15 or longer for BLAST lookup
+-i      also keep intermediate files    
 "
+
 else
-    # go up a directory to access all the awk programs
-    #cd ..
     # will run all the awk files together
     # extracts, finds the genes, translates into peptide sequence
     awk -f extract.awk $1 | awk -f isolate_subsequences.awk > genes.txt
+
     # extracts, converts to reverse compliment, finds the genes, translates into peptide sequence
     awk -f extract.awk $1 | awk -f reverse_compliment.awk | awk -f isolate_subsequences.awk >> genes.txt
+    
     # convert to peptides
     awk -f translate.awk genes.txt > peptides.txt
-    # sort by size
-    cat genes.txt | awk '{ print length, $0 }' | sort -n | awk '{$1=""; print $0}' > sorted_genes.txt
-    cat peptides.txt | awk '{ print length, $0 }' | sort -n | awk '{$1=""; print $0}' > sorted_peptides.txt
-    # reverse the files
     
-    # combine the files into the order tony specified
+    # sort by size, reversed
+    cat genes.txt | awk '{ print length, $0 }' | sort -nr | awk '{$1=""; print $0}' > sorted_genes.txt
+    cat peptides.txt | awk '{ print length, $0 }' | sort -nr | awk '{$1=""; print $0}' > sorted_peptides.txt
     
-    # peptide sequence that are 15 characters long or greater, for blasting
-    # awk -f size_filter.awk peptides.txt > final.txt
-
-    rm genes.txt
-    rm peptides.txt
-    rm sorted_genes.txt
-    rm sorted_peptides.txt
+    # combine the files into gene, protien, newline
+    awk 'BEGIN { OFS="\n"} FNR==NR { a[(FNR"")] = $0; next } { print a[(FNR"")], $0, ""}' sorted_genes.txt sorted_peptides.txt > ../results/corresponding_genes_peptides.txt
+    
+    if [ "$2" == '-l' ]
+    then
+        # peptide sequence that are 15 characters long or greater, for blasting
+        awk -f size_filter.awk peptides.txt | uniq > ../results/peptides_for_lookup.txt
+    elif [ "$2" == '-i' ]
+    then
+        # move intermediate files into results folder
+        mv genes.txt peptides.txt sorted_genes.txt sorted_peptides.txt ../results
+    else
+        # otherwise remove
+        rm genes.txt peptides.txt sorted_genes.txt sorted_peptides.txt
+    fi
 fi
